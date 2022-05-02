48c23096d39dcbd05274d1846d678aeeed60f79f
amelieBNPP <amelie.noury@bnpparibas.com>
2022-05-02 14:50:47 +0200

    :bird: migration: update relations between models

--- epicevents/models.py
+++ epicevents/models.py
@@ -11,7 +11,7 @@ ROLE = (
 class Employee(models.Model):
     function = models.CharField(max_length=250)
     role = models.CharField(choices=ROLE, max_length=25)
-    employee_contact = models.OneToOneField(
+    employee_contact = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
@@ -24,11 +24,11 @@ class Client(models.Model):
     compagny_name = models.CharField(max_length=250)
     last_contact = models.DateTimeField(auto_now=True)
     status = models.BooleanField()
-    client_contact = models.OneToOneField(
+    client_contact = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    sales_contact = models.OneToOneField(
+    sales_contact = models.ForeignKey(
         to=Employee,
         on_delete=models.CASCADE,
     )
